f13a06fdb0cd5a1bd08e1b206f3108589fb2b96e
2ma1995 <vthquddbsv@gmail.com>
2024-02-14 22:38:01 +0900

    20240214

--- D-Day_counter/main.js
+++ D-Day_counter/main.js
@@ -2,12 +2,17 @@ const dateFormMaker = function () {
   const inputYear = document.querySelector(`#target-year-input`).value;
   const inputMonth = document.querySelector(`#target-month-input`).value;
   const inputDate = document.querySelector(`#target-date-input`).value;
-  console.log(inputYear, inputMonth, inputDate);
+
+  // const dateFormat = inputYear + `-` + inputMonth + `-` + inputDate;
+  const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`;
+  // console.log(inputYear, inputMonth, inputDate);
+  return dateFormat;
 };
 
 const counterMake = function () {
+  const targetDateInput = dateFormMaker();
   const nowDate = new Date();
-  const targetDate = new Date(`2024-08-15`);
+  const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
   const remaining = (targetDate - nowDate) / 1000;
 
   const remainingDate = Math.floor(remaining / 3600 / 24);
@@ -17,3 +22,6 @@ const counterMake = function () {
 
   console.log(remainingDate, remainingHours, remainingMin, remainingsec);
 };
+/**
+ *
+ */
